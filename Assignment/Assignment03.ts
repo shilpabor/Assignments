003e0e6b5c43aaa0a8c72127970ea8e2594468a9
shilpabor <shilpaborkar294@gamil.com>
2026-02-15 13:29:23 +0530

    Assignment 03 corrected

--- Assignment/Assignment03.ts
+++ Assignment/Assignment03.ts
@@ -1,10 +1,15 @@
 let studentname:string[]=["Suresh","Mahesh","Naresh"];
 let marks:number[]=[75, 80, 82]
 
-let Suresh=marks[0]+=10
+/* let Suresh=marks[0]+=10
 let Mahesh=marks[1]+=10
-let Naresh=marks[2]+=10
+let Naresh=marks[2]+=10 */
 
-console.log("updated Marks:\n"+studentname[0]+": "+Suresh+"\n"+studentname[1]+": "+Mahesh+"\n"+studentname[2]+": "+Naresh)
+for(let i:number=0;i<marks.length;i++)
+{
+    marks[i]+=10
+  console.log("updated Marks:\n"+studentname[i]+": "+marks[i])
+}
+//console.log("updated Marks:\n"+studentname[0]+": "+marks[i]+"\n"+studentname[1]+": "+marks[i]+"\n"+studentname[2]+": "+marks[i])
 
-console.log("Average Marks:"+(Suresh+Mahesh+Naresh)/3)
+//console.log("Average Marks:"+(Suresh+Mahesh+Naresh)/3)
